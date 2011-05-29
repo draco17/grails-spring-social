4c2b7081bba669e89d92d74da0833c7e1a4ac94b
Domingo Suarez Torres <domingo.suarez@gmail.com>
2011-05-29 17:58:44 -0500

    fixed url mappings

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -15,17 +15,9 @@
 class UrlMappings {
 
     static mappings = {
-        "/$controller/$action?/$id?" {
-            constraints {
-                // apply constraints here
-            }
-        }
 
-        "/"(view: "/index")
-        "500"(view: '/error')
-
-        name springSocialconnect: "/ssconnect/${providerId}" {
-            controller = 'springSocialconnect'
+        name springSocialConnect: "/ssconnect/${providerId}" {
+            controller = 'springSocialConnect'
             action = [GET: "oauthCallback", POST: 'withProvider', DELETE: "disconnect",]
         }
 
